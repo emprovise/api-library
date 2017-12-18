6db4e00c5d3698cc91f3a965b7db67ec554171d3
Pranav Patil <pranavpatil.cs@gmail.com>
2017-12-17 18:35:11 -0600

    Adding Java mail test, removing main methods

--- google-api/src/main/java/com/emprovise/api/google/gmail/JavaMail.java
+++ google-api/src/main/java/com/emprovise/api/google/gmail/JavaMail.java
@@ -101,18 +101,4 @@ public class JavaMail {
         }
         return result;
     }
-
-    public static void main(String[] args) throws Exception {
-
-        JavaMail javaMail = new JavaMail();
-        List<EmailMessage> emailMessages = javaMail.fetchEmails("emailaddress@gmail.com", "password");
-
-        for (EmailMessage emailMessage : emailMessages) {
-            System.out.println("---------------------------------");
-            System.out.println("Email Number " + emailMessage.getMessageId());
-            System.out.println("Subject: " + emailMessage.getSubject());
-            System.out.println("From: " + emailMessage.getSender());
-            System.out.println("Body: " + emailMessage.getContent());
-        }
-    }
 }
